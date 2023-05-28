4551f72d6152e82c08ef4f0b232cee5a14cda105
incava <68988975+incava@users.noreply.github.com>
2023-05-28 17:03:39 +0900

    Initial Commit

---
+++ transaction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  transaction
+//
+//  Created by ingi choi on 2023/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
